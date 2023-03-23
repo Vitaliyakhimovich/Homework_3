0ad66b2007d824908cb531d94dbe063c21ada69e
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-23 17:33:33 +0300

    add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите значения: ");
+double xb = Convert.ToInt32(Console.ReadLine());
+double xa = Convert.ToInt32(Console.ReadLine());
+double yb = Convert.ToInt32(Console.ReadLine());
+double ya = Convert.ToInt32(Console.ReadLine());
+double zb = Convert.ToInt32(Console.ReadLine());
+double za = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow((xb - xa), 2) + Math.Pow((yb - ya), 2) + Math.Pow((zb - za), 2) );
+
+Console.WriteLine(result);
